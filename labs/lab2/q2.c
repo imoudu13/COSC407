f87b39743dc4ce08158a77e9e4775eee83255d09
imoudu13 <imouduibrahim11@gmail.com>
2025-01-21 13:29:21 -0800

    finished q2

---
+++ labs/lab2/q2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* addVec2(int* A, int* B, int size);
+
+int main() {
+    const int FIFTY_MILLION = 50000000;
+    int* A = calloc(FIFTY_MILLION, sizeof(int));
+
+    if (A == NULL) {
+        printf("Error with memory allocation for A.\n");
+        return 1; // return 1 for error
+    }
+
+    int* B = calloc(FIFTY_MILLION, sizeof(int));
+
+    if (B == NULL) {
+        printf("Error with memory allocation for B.\n");
+        return 1; // return 1 for error
+    }
+
+    int* C = addVec2(A, B, FIFTY_MILLION);
+
+    for (int i = 0; i < 10; i++) {
+        if (i < 10) {
+            printf("C[%d] = %d\n", i, C[i]);
+        }
+    }
+
+    free(A);
+    free(B);
+    free(C);
+
+    return 0;
+}
+
+int* addVec2(int* A, int* B, int size) {
+    int* C = calloc(size, sizeof(int));
+
+    if (C == NULL) {
+        printf("Memory allocation of C failed\n");
+    }
+
+    for (int i = 0; i < size; i++) {
+        C[i] = A[i] + B[i];
+    }
+
+    return C;
+}
