79b90ebb0973341bde5bd68c5f68f33d3060556e
imoudu13 <imouduibrahim11@gmail.com>
2025-01-27 22:25:37 -0800

    finished assignment

--- labs/lab2/q3.c
+++ labs/lab2/q3.c
@@ -4,18 +4,15 @@
 int main(int argc, char *argv[]) {
     int numThreads, tid;
 
-    /* This creates a team of threads; each thread has its own copy of variables */
     #pragma omp parallel private(tid) shared(numThreads) 
     {
         tid = omp_get_thread_num();
         
-        /* Synchronize output for clarity */
         #pragma omp critical
         {
             printf("Hello World from thread number %d\n", tid);
         }
 
-        /* The following is executed by the master thread only (tid=0) */
         if (tid == 0) {
             numThreads = omp_get_num_threads();
             printf("Number of threads is %d\n", numThreads);
---
+++ labs/lab2/q4.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+
+int* vecCreate(int size);
+int* vecCreateOpenMP(int size, int num_thread);
+
+int main() {
+    const int SIZE = 50000000; // 50 million elements
+    int num_threads = 4;
+
+    // sequential implementation
+    clock_t start_seq = clock();
+    int* sequential = vecCreate(SIZE);
+    clock_t end_seq = clock();
+
+    if (sequential != NULL) {
+        printf("Sequential: Last element: %d\n", sequential[SIZE - 1]);
+        double time_taken = (double)(end_seq - start_seq) / CLOCKS_PER_SEC;
+        printf("Sequential: Time taken: %.2f seconds\n", time_taken);
+        free(sequential);
+    }
+
+    clock_t start_par = omp_get_wtime();
+    int* parallel = vecCreateOpenMP(SIZE, num_threads);
+    clock_t end_par = omp_get_wtime();
+
+    if (parallel != NULL) {
+        printf("Parallel: Last element: %d\n", parallel[SIZE - 1]);
+        double time_taken = (double)(end_par - start_par) / CLOCKS_PER_SEC;
+        printf("Parallel: Time taken: %.2f seconds\n", time_taken);
+        free(parallel);
+    }
+
+    return 0;
+}
+
+int* vecCreate(const int SIZE) {
+    int* A = malloc(SIZE * sizeof(int));
+
+    if (A == NULL) {
+        printf("There was a problem allocating memory to create the array of size: %d\n", SIZE);
+        return NULL;
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        A[i] = i;
+    }
+
+    return A;
+}
+
+int* vecCreateOpenMP(const int SIZE, int thread_count) {
+    if (SIZE % thread_count != 0) {
+        printf("Error: Vector size must be divisible by the number of threads.\n");
+        return NULL;
+    }
+
+    int* A = malloc(SIZE * sizeof(int));
+
+    if (A == NULL) {
+        printf("There was a problem allocating memory to create the array of size: %d\n", SIZE);
+        return NULL;
+    }
+
+    # pragma omp parallel num_threads(thread_count)
+    {
+        int thread_id = omp_get_thread_num();
+        int chunk_size = SIZE / thread_count;
+        int start = chunk_size * thread_id;
+        int end = start + chunk_size;
+
+        for (int i = start; i < end; i++) {
+            A[i] = i;
+        }
+    }
+    
+    return A;
+}
