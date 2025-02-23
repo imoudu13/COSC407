b10b90e13bb556e48e21a009962a802c55d294e8
Imoudu Ibrahim <72046339+imoudu13@users.noreply.github.com>
2025-02-22 19:41:42 -0800

    finished q3

--- labs/lab4/q3.c
+++ labs/lab4/q3.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <omp.h>
 
-int main() {
-    int n = 10; // Example size, you can change it
-    int a[n];
+#define N 10
 
-    // Parallel computation of the sequence
-    #pragma omp parallel for
-    for (int i = 0; i < n; i++) {
-        a[i] = (i * (i + 1)) / 2;
+int main() {
+    int a[N];
+    
+    // using the ordered clause fixes the loop carried dependeny
+    // you could also try reworking the algorithm 
+    a[0] = 0;
+    #pragma omp parallel for ordered
+    for (int i = 1; i < N; i++) {
+        #pragma omp ordered
+        a[i] = a[i - 1] + i;
     }
 
-    // Print the results
-    printf("Computed array:\n");
-    for (int i = 0; i < n; i++) {
+    // Print results
+    for (int i = 0; i < N; i++) {
         printf("%d ", a[i]);
     }
     printf("\n");
